8e0bf1af759a404cf84577be7b00d1c437aee479
Leigh Hobson <leighhobson89@aol.com>
2023-04-13 19:07:02 +0200

    MoreCountriesAddedToSVG

--- index.js
+++ index.js
@@ -1,89 +1,105 @@
 function svgMapLoaded() {
-    console.log("page loaded!");
-  
-    const svgMap = document.getElementById('svg-map').contentDocument;
-    const tooltip = document.getElementById("tooltip");
-  
-    // Add a mouseover event listener to the SVG element
-    svgMap.addEventListener("mouseover", function(e) {
-      // Get the path element that was hovered over
-      const path = e.target;
-  
-      // Get the name of the country from the "data-name" attribute
-      const countryName = path.getAttribute("data-name");
-  
-      // Get the coordinates of the mouse cursor
-      const x = e.clientX;
-      const y = e.clientY;
-  
-      // Set the content of the tooltip
-      tooltip.innerHTML = countryName;
-  
-      // Position the tooltip next to the mouse cursor
-      tooltip.style.left = x + "px";
-      tooltip.style.top = y + "px";
-  
-      // Show the tooltip
-      tooltip.style.display = "block";
-    });
-  
-    // Add a mouseout event listener to the SVG element
-    svgMap.addEventListener("mouseout", function(e) {
-      // Hide the tooltip
-      tooltip.innerHTML = "";
-      tooltip.style.display = "none";
-    });
+  console.log("page loaded!");
 
-    svgMap.addEventListener("click", function(e) {
-        const path = e.target;
-        sendPostRequest(path.getAttribute("data-name"));
-      });
-  }
-  
-  window.addEventListener('load', function() {
-    svgMapLoaded();
+  const svgMap = document.getElementById('svg-map').contentDocument;
+  const tooltip = document.getElementById("tooltip");
+
+  // Add a mouseover event listener to the SVG element
+  svgMap.addEventListener("mouseover", function(e) {
+    // Get the path element that was hovered over
+    const path = e.target;
+
+    // Get the name of the country from the "data-name" attribute
+    const countryName = path.getAttribute("data-name");
+
+    // Get the coordinates of the mouse cursor
+    const x = e.clientX;
+    const y = e.clientY;
+
+    // Set the content of the tooltip
+    tooltip.innerHTML = countryName;
+
+    // Position the tooltip next to the mouse cursor
+    tooltip.style.left = 10 + x + "px";
+    tooltip.style.top = y + "px";
+
+    // Show the tooltip
+    tooltip.style.display = "block";
+    tooltip.style.backgroundColor = "white";
   });
 
-  function sendPostRequest(country) {
-      // Ensure hovering over a country before sending request
-    if (tooltip.innerHTML === "") {
-      return;
-    }
+  // Add a mouseout event listener to the SVG element
+  svgMap.addEventListener("mouseout", function(e) {
+    // Hide the tooltip
+    tooltip.innerHTML = "";
+    tooltip.style.display = "none";
+    tooltip.style.backgroundColor = "transparent";
+  });
 
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://localhost:8000/databaseConn.php", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function () {
-      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        // Parse the response JSON data
-        const data = JSON.parse(xhr.responseText);
-        console.log(data);
-  
-        // Update the table with the response data
-        document.getElementById("my-table").rows[0].cells[0].innerHTML = data[0].country + " (" + data[0].continent + ")";
-        /* document.getElementById("my-table").rows[1].cells[1].innerHTML = data[0].continent; */
-       
-        if (data[0].startingPop.length > 0) {
-          const population = formatPopulation(data[0].startingPop);
-          document.getElementById("my-table").rows[1].cells[1].innerHTML = population;
-        }
-        document.getElementById("my-table").rows[2].cells[1].innerHTML = data[0].area;
-        document.getElementById("my-table").rows[3].cells[1].innerHTML = data[0].startingArmy;
-      }
-    };
-    xhr.send("country=" + country);
-  }
-  
-  function formatPopulation(population) {
-    if (population >= 1000000000) {
-      return (population / 1000000000).toFixed(1) + 'B';
-    } else if (population >= 1000000) {
-      return (population / 1000000).toFixed(1) + 'M';
-    } else if (population >= 1000) {
-      return (population / 1000).toFixed(1) + 'k';
+  // Add a mousemove event listener to the SVG element
+  svgMap.addEventListener("mousemove", function(e) {
+    // Check if the mouse is currently over a country with a tooltip
+    if (tooltip.innerHTML !== "") {
+      // Set the display property of the tooltip to "block" to show it
+      tooltip.style.display = "block";
+      // Set the background color of the tooltip to white
+      tooltip.style.backgroundColor = "white";
     } else {
-      return population.toString();
+      // Set the display property of the tooltip to "none" to hide it
+      tooltip.style.display = "none";
+      // Set the background color of the tooltip to transparent
+      tooltip.style.backgroundColor = "transparent";
     }
+  });
+
+  svgMap.addEventListener("click", function(e) {
+      const path = e.target;
+      sendPostRequest(path.getAttribute("data-name"));
+    });
+}
+
+window.addEventListener('load', function() {
+  svgMapLoaded();
+});
+
+function sendPostRequest(country) {
+    // Ensure hovering over a country before sending request
+  if (tooltip.innerHTML === "") {
+    return;
   }
-  
-  
+
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "http://localhost:8000/databaseConn.php", true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function () {
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      // Parse the response JSON data
+      const data = JSON.parse(xhr.responseText);
+      console.log(data);
+
+      // Update the table with the response data
+      document.getElementById("my-table").rows[0].cells[0].style.whiteSpace = "pre";
+      document.getElementById("my-table").rows[0].cells[0].innerHTML = data[0].country + " (" + data[0].continent + ")";
+     
+      if (data[0].startingPop.length > 0) {
+        const population = formatPopulation(data[0].startingPop);
+        document.getElementById("my-table").rows[0].cells[2].innerHTML = population;
+      }
+      document.getElementById("my-table").rows[0].cells[4].innerHTML = data[0].area;
+      document.getElementById("my-table").rows[0].cells[6].innerHTML = data[0].startingArmy;
+    }
+  };
+  xhr.send("country=" + country);
+}
+
+function formatPopulation(population) {
+  if (population >= 1000000000) {
+    return (population / 1000000000).toFixed(1) + 'B';
+  } else if (population >= 1000000) {
+    return (population / 1000000).toFixed(1) + 'M';
+  } else if (population >= 1000) {
+    return (population / 1000).toFixed(1) + 'k';
+  } else {
+    return population.toString();
+  }
+}
