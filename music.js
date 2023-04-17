11e2b0d5b1b6b4230fe7360a2290c91a526f4e74
Leigh Hobson <leighhobson89@aol.com>
2023-04-18 01:14:06 +0200

    music fixed, lots of other fixes, seems stable now.

--- music.js
+++ music.js
@@ -1,45 +1,59 @@
 // create an audio element and add the source of the music file
 const audio = new Audio('resources/musicGame.mp3');
 
-// keep track of whether the music is currently playing or not
-let isPlaying = true;
+let isPlaying;
 
 // define the currentTime variable
 let currentTime = 0;
 
 document.addEventListener("DOMContentLoaded", function() {
-    // play the audio when the DOM content is loaded
-    audio.play();
 
     const musicButton = document.getElementById("toggle-music-btn");
     const newGameButton = document.getElementById("new-game-btn");
+    let firstTime;
     if (musicButton) {
         musicButton.addEventListener("click", function() {
-            // toggle the playing status
-            isPlaying = !isPlaying;
-
-            if (isPlaying) {
+            if (isPlaying == undefined) {
+                firstTime = true;
                 document.getElementById("toggle-music-btn").classList.add("isPlaying");
-                // if the music is now playing, set the current time and play the audio
-                audio.currentTime = currentTime;
+                isPlaying = true;
                 audio.play();
-            } else {
-                document.getElementById("toggle-music-btn").classList.remove("isPlaying");
-                // if the music is now paused, pause the audio and save the current time
-                audio.pause();
-                currentTime = audio.currentTime;
             }
+            
+            if (!firstTime) {
+                isPlaying = !isPlaying;
+                if (isPlaying) {
+                    document.getElementById("toggle-music-btn").classList.remove("isNotPlaying");
+                    document.getElementById("toggle-music-btn").classList.add("isPlaying");
+                    // if the music is now playing, set the current time and play the audio
+                    audio.currentTime = currentTime;
+                    audio.play();
+                } else {
+                    document.getElementById("toggle-music-btn").classList.remove("isPlaying");
+                    document.getElementById("toggle-music-btn").classList.add("isNotPlaying");
+                    // if the music is now paused, pause the audio and save the current time
+                    audio.pause();
+                    currentTime = audio.currentTime;
+                }
+            }
+            firstTime = false;
+
         });
     }
 
     if (newGameButton) {
         newGameButton.addEventListener("click", function() {
-
-            if (isPlaying) {
-                // if the music is now playing, set the current time and play the audio
-                audio.currentTime = currentTime;
+            if (isPlaying == undefined) {
+                document.getElementById("toggle-music-btn").classList.add("isPlaying");
+                isPlaying = true;
                 audio.play();
             }
         });
     }
+
+    audio.addEventListener("ended", function() {
+        // Set the current time to 0 and play the audio to loop it
+        audio.currentTime = 0;
+        audio.play();
+    });
 });
