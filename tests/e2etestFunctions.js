e67632ef09d6b31b4d86cc915b528967378d1c90
Leigh Hobson <leighhobson89@aol.com>
2023-07-05 12:47:43 +0200

    added large function to click through the battle

--- tests/e2etestFunctions.js
+++ tests/e2etestFunctions.js
@@ -54,7 +54,11 @@ async function clickNewGame(driver) {
 }
 
 async function clickPlayerCountryPath(driver, path) {
-  // Find the path element with attribute "uniqueid" equal to "path"
+  await switchContext(driver, "default");
+  const tooltipElement = await driver.findElement(By.id('tooltip'));
+  const originalDisplay = await tooltipElement.getCssValue('display');
+  await driver.executeScript("arguments[0].style.display = 'none';", tooltipElement);
+  await switchContext(driver, "svg");
   let pathElement = await driver.findElement(By.css('path[uniqueid="' + path + '"]'));
   if (pathElement) {
     await pathElement.click();
@@ -62,7 +66,11 @@ async function clickPlayerCountryPath(driver, path) {
   } else {
     console.log('Could not find path element ' + path);
   }
+  await switchContext(driver, "default");
+  await driver.executeScript("arguments[0].style.display = arguments[1];", tooltipElement, originalDisplay);
+  await switchContext(driver, "svg");
 }
+
 
 async function clickPopupConfirm(driver, situation) {
   // Find the element with id="popup-confirm"
@@ -186,11 +194,191 @@ async function addMaxArmy(driver) {
     col = await driver.findElement(By.css(`.transfer-table-row:first-child .army-type-column:nth-child(${i}) .transferPlusButton`));
     await col.click();
     await wait(100);
-    amountsArray[i] = await driver.findElement(By.css(`.transfer-table-row:first-child .army-type-column:nth-child(${i}) .quantityTextField`));
-    console.log("Added " + amountsArray[i] + " for column " + i);
+    amountsArray[i-1] = await driver.findElement(By.css(`.transfer-table-row:first-child .army-type-column:nth-child(${i}) .quantityTextField`)).getAttribute('value');
+    console.log("Added " + amountsArray[i-1] + " for column " + i);
   }
   return amountsArray;
 }
+
+async function validateBattleUI(driver) {
+  const leftTitle = await driver.findElement(By.id('battleUITitleTitleLeft')).getText();
+  const centerTitle = await driver.findElement(By.id('battleUITitleTitleCenter')).getText();
+  const rightTitle = await driver.findElement(By.id('battleUITitleTitleRight')).getText();
+
+  console.log("The war is " + leftTitle + " " + centerTitle + " " + rightTitle);
+
+  const attInfantry = await driver.findElement(By.id('armyRowRow2Quantity1')).getText();
+  const attAssault = await driver.findElement(By.id('armyRowRow2Quantity2')).getText();
+  const attAir = await driver.findElement(By.id('armyRowRow2Quantity3')).getText();
+  const attNaval = await driver.findElement(By.id('armyRowRow2Quantity4')).getText();
+
+  const defInfantry = await driver.findElement(By.id('armyRowRow2Quantity5')).getText();
+  const defAssault = await driver.findElement(By.id('armyRowRow2Quantity6')).getText();
+  const defAir = await driver.findElement(By.id('armyRowRow2Quantity7')).getText();
+  const defNaval = await driver.findElement(By.id('armyRowRow2Quantity8')).getText();
+
+  const probability = await driver.findElement(By.id('battleUIRow4Col1')).getText();
+  const defenceBonus = await driver.findElement(By.id('defenceBonusText')).getText();
+
+  const probabilityColumnBox = await driver.findElement(By.id('probabilityColumnBox'));
+  const widthValueProbabilityColumnBox = await probabilityColumnBox.getCssValue('width');
+
+  console.log("Carrying out Attack, starting with " + attInfantry + " infantry, " + attAssault + " assault, " + attAir + " air, and " + attNaval + " naval!");
+  console.log("Defenders have " + defInfantry + " infantry, " + defAssault + " assault, " + defAir + " air, and " + defNaval + " naval!");
+  console.log(probability);
+  console.log("The width value of probabilityColumnBox is: " + widthValueProbabilityColumnBox);
+  console.log("Defence Bonus: " + defenceBonus);
+
+  const siegeButton = await driver.findElement(By.id('siegeButton'));
+  const advanceButton = await driver.findElement(By.id('advanceButton'));
+  const retreatButton = await driver.findElement(By.id('retreatButton'));
+
+  const siegeDisabled = await siegeButton.getAttribute('disabled');
+  const siegeText = await siegeButton.getAttribute('innerHTML');
+  const advanceDisabled = await advanceButton.getAttribute('disabled');
+  const advanceText = await advanceButton.getAttribute('innerHTML');
+  const retreatDisabled = await retreatButton.getAttribute('disabled');
+  const retreatText = await retreatButton.getAttribute('innerHTML');
+
+  console.log("siegeButton - Disabled: " + siegeDisabled + ", Text: " + siegeText);
+  console.log("advanceButton - Disabled: " + advanceDisabled + ", Text: " + advanceText);
+  console.log("retreatButton - Disabled: " + retreatDisabled + ", Text: " + retreatText);
+}
+
+async function clickThroughAttack(driver) {
+  const advanceButton = await driver.findElement(By.id('advanceButton'));
+  const retreatButton = await driver.findElement(By.id('retreatButton'));
+  const siegeButton = await driver.findElement(By.id('siegeButton'));
+
+  let advanceButtonText = await advanceButton.getText();
+  let retreatButtonText = await retreatButton.getText();
+  let siegeButtonDisabled = await siegeButton.getAttribute('disabled');
+  let retreatButtonDisabled = await retreatButton.getAttribute('disabled');
+  let advanceButtonDisabled = await advanceButton.getAttribute('disabled');
+
+  console.log('Advance Button:', advanceButtonText);
+  console.log('Retreat Button:', retreatButtonText);
+  console.log('Siege Button Disabled:', siegeButtonDisabled);
+  console.log('Retreat Button Disabled:', retreatButtonDisabled);
+  console.log('Advance Button Disabled:', advanceButtonDisabled);
+
+  if (advanceButtonText === 'Begin War!' && retreatButtonText === 'Retreat!') {
+    await advanceButton.click();
+    console.log('Clicked Advance Button');
+  }
+
+  let battleResolution = '';
+
+  while (true) {
+    advanceButtonText = await advanceButton.getText();
+    retreatButtonText = await retreatButton.getText();
+    siegeButtonDisabled = await siegeButton.getAttribute('disabled');
+    retreatButtonDisabled = await retreatButton.getAttribute('disabled');
+    advanceButtonDisabled = await advanceButton.getAttribute('disabled');
+
+    await wait(200);
+    if (advanceButtonText === 'Next Skirmish') {
+      await advanceButton.click();
+      console.log('Clicked Next Skirmish');
+
+      advanceButtonText = await advanceButton.getText();
+      retreatButtonText = await retreatButton.getText();
+      siegeButtonDisabled = await siegeButton.getAttribute('disabled');
+      retreatButtonDisabled = await retreatButton.getAttribute('disabled');
+      advanceButtonDisabled = await advanceButton.getAttribute('disabled');
+
+      if (advanceButtonText === 'Massive Assault' || advanceButtonText === 'Rout The Enemy' || advanceButtonText === 'Victory!') {
+        console.log('Battle Resolution Available:', advanceButtonText);
+        battleResolution = advanceButtonText;
+        await wait(500);
+        break;
+      } else if (advanceButtonText === 'End Round' && retreatButtonText === 'Scatter!' && siegeButtonDisabled && retreatButtonDisabled) {
+          console.log('End of Round, Continuing...');
+          await advanceButton.click();
+          await wait(200);
+          continue;
+      }
+    } else if (retreatButtonText === 'Retreat!' && advanceButtonText === 'Start Attack!' && !advanceButtonDisabled && !siegeButtonDisabled) {
+        await advanceButton.click();
+        console.log('Clicked Start Attack!');
+        continue;
+    }
+
+    advanceButtonText = await advanceButton.getText();
+    retreatButtonText = await retreatButton.getText();
+    siegeButtonDisabled = await siegeButton.getAttribute('disabled');
+    retreatButtonDisabled = await retreatButton.getAttribute('disabled');
+    advanceButtonDisabled = await advanceButton.getAttribute('disabled');
+
+    console.log('Advance Button:', advanceButtonText);
+    console.log('Retreat Button:', retreatButtonText);
+    console.log('Siege Button Disabled:', siegeButtonDisabled);
+    console.log('Retreat Button Disabled:', retreatButtonDisabled);
+    console.log('Advance Button Disabled:', advanceButtonDisabled);
+
+    if (advanceButtonText === 'Next Skirmish') {
+      if (retreatButtonText === 'Scatter!') {
+        console.log('Retreat Button says "Scatter!"');
+      }
+
+      if (retreatButtonText === 'Defeat!') {
+        console.log('Retreat Button says "Defeat!"');
+        break;
+      }
+    }
+    await wait(300);
+  }
+
+  advanceButtonText = await advanceButton.getText();
+  retreatButtonText = await retreatButton.getText();
+  siegeButtonDisabled = await siegeButton.getAttribute('disabled');
+  retreatButtonDisabled = await retreatButton.getAttribute('disabled');
+  advanceButtonDisabled = await advanceButton.getAttribute('disabled');
+
+  let armyQuantities = [];
+  let enemyArmyQuantities = [];
+
+  if (advanceButtonDisabled && siegeButtonDisabled && retreatButtonText === 'Defeat!') {
+    console.log('Advance Button Disabled, Siege Button Disabled, Retreat Button says "Defeat!"');
+    battleResolution = 'D';
+  }
+
+  if (retreatButtonDisabled && siegeButtonDisabled && advanceButtonText === 'Rout The Enemy') {
+    console.log('Retreat Button Disabled, Siege Button Disabled, Retreat Button says "Rout The Enemy"');
+    battleResolution = 'VR';
+  }
+
+  if (retreatButtonDisabled && siegeButtonDisabled && advanceButtonText === 'Massive Assault') {
+    console.log('Retreat Button Disabled, Siege Button Disabled, Retreat Button says "Massive Assault"');
+    battleResolution = 'VMA';
+  }
+
+  if (retreatButtonDisabled && siegeButtonDisabled && advanceButtonText === 'Victory!') {
+    console.log('Retreat Button Disabled, Siege Button Disabled, Retreat Button says "Victory!"');
+    battleResolution = 'V';
+  }
+
+  const attInfantry = await driver.findElement(By.id('armyRowRow2Quantity1')).getText();
+  const attAssault = await driver.findElement(By.id('armyRowRow2Quantity2')).getText();
+  const attAir = await driver.findElement(By.id('armyRowRow2Quantity3')).getText();
+  const attNaval = await driver.findElement(By.id('armyRowRow2Quantity4')).getText();
+
+  const defInfantry = await driver.findElement(By.id('armyRowRow2Quantity5')).getText();
+  const defAssault = await driver.findElement(By.id('armyRowRow2Quantity6')).getText();
+  const defAir = await driver.findElement(By.id('armyRowRow2Quantity7')).getText();
+  const defNaval = await driver.findElement(By.id('armyRowRow2Quantity8')).getText();
+
+  armyQuantities = [attInfantry, attAssault, attAir, attNaval];
+  enemyArmyQuantities = [defInfantry, defAssault, defAir, defNaval];
+  result = [armyQuantities, enemyArmyQuantities, battleResolution];
+
+  console.log("Attackers Remaining " + armyQuantities.join(', ') + "!");
+  console.log("Defenders remaining " + enemyArmyQuantities.join(', ') + "!");
+  console.log("Resolution of the Battle: " + battleResolution);
+
+  return result;
+}
+
 
 async function wait(time) {
   await new Promise(resolve => setTimeout(resolve, time));
@@ -206,6 +394,8 @@ module.exports = {
   selectRandomCountryToAttack: selectRandomCountryToAttack,
   clickAttackTransferButton: clickAttackTransferButton,
   validateAttackTransferWindowOpen: validateAttackTransferWindowOpen,
-  addMaxArmy: addMaxArmy
+  addMaxArmy: addMaxArmy,
+  validateBattleUI: validateBattleUI,
+  clickThroughAttack: clickThroughAttack
 };
 
