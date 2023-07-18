225261d0604c88e5455dd25fddb2c2ee96310533
Leigh Hobson <leighhobson89@aol.com>
2023-07-18 15:00:38 +0200

    added bufferGeometryUtils config to build

---
+++ webpack-buffer-utils.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        bufferutils: './node_modules/three/examples/jsm/utils/BufferGeometryUtils.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bufferutils-bundle.js',
+        library: 'BufferUtilsGeometry',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+};
